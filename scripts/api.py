29eeebaa03db22c6c18dafe5e43aa5ad85bd67d8
marcogemaque <marcogemaque@id.uff.br>
2023-12-23 18:35:35 +0000

    changed table reference

--- scripts/api.py
+++ scripts/api.py
@@ -54,9 +54,8 @@ def query_team_aliases():
     Queries the ALIASES for each team (with UUID)
     """
     connection, cursor = connect_to_db()
-    query = "select * from pg_catalog.pg_tables;"#"SELECT * FROM teams_aliases left join team_keys using(uuid);"
+    query = "SELECT * FROM teams_aliases left join team_keys using(uuid);"
     team_urls_to_query = pd.read_sql_query(query, con=connection)
-    print(team_urls_to_query)
     team_uuids = team_urls_to_query["uuid"].tolist()
     team_aliases = team_urls_to_query["alias"].tolist()
     #flatten the list
